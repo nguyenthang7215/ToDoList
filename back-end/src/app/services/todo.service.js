8e235c9d2133712db4101f8c98d3f3a9c164bfb9
Nguyen Thang <nguyenthang7215@gmail.com>
2025-09-30 01:28:27 +0700

    Task 2: Design DB schema & Implement model & Seed database

---
+++ back-end/src/app/services/todo.service.js
@@ -0,0 +1,101 @@
+import pool from '../../configs/postgres.js';
+
+// Create new todo 
+export async function createTodo({ title, description, status, due_date, user_id }) {
+    try {
+        const queryText = `
+            insert into todos (title, description, status, due_date, user_id)
+            values ($1, $2, $3, $4, $5)
+            returning *
+        `;
+        const result = await pool.query(queryText, [title, description, status, due_date, user_id]);
+        return result.rows[0];
+    } catch (error) {
+        throw new Error('Lỗi tạo todo list:', error.message);
+    }
+}
+
+// Find todo by id
+export async function findTodoById(id) {
+    try {
+        const queryText = `
+            select * from todos 
+            where id = $1
+        `;
+        const result = await pool.query(queryText, [id]);
+        if (result.rows.length === 0) {
+            return null;
+        }
+        return result.rows[0];
+    } catch (error) {
+        throw new Error('Lỗi không tìm được todo bang id:', error.message);
+    }
+}
+
+// Get all todo 
+export async function getAllTodo(userId) {
+    try {
+        const queryText = `
+            select * from todos where user_id = $1
+        `;
+        const result = await pool.query(queryText, [userId]);
+        return result.rows;
+    } catch (error) {
+        throw new Error('Lỗi không lấy được tất cả todo list:', error.message);
+    }
+}
+
+// Update todo (title, description, status, due_date)
+export async function updateTodo(id, { title, description, status, due_date }) {
+    try {
+        const currentTodo = await findTodoById(id);
+        if (!currentTodo) {
+            throw new Error('Id không hợp lệ');
+        }
+        const queryText = `
+            update todos
+            set title = $1, description = $2, status = $3, due_date = $4
+            where id = $5
+            returning *
+        `;
+        const result = await pool.query(queryText, [title, description, status, due_date, id]);
+        return result.rows[0];
+    } catch (error) {
+        throw new Error('Lỗi không cập nhật được todo list:', error.message);
+    }
+}
+
+// Toggle status (pending and done)
+export async function toggleStatus(id) {
+    try {
+        const currentTodo = await findTodoById(id);
+        if (!currentTodo) {
+            throw new Error('Id không hợp lệ');
+        }
+        const newStatus = currentTodo.status === 'pending' ? 'done' : 'pending';
+
+        const queryText = `
+            update todos
+            set status = $1
+            where id = $2
+            returning *
+        `;
+        const result = await pool.query(queryText, [newStatus, id]);
+        return result.rows[0];
+    } catch (error) {
+        throw new Error('Lỗi thay đôi trạng thái:', error.message);
+    }
+}
+
+// Delete todo 
+export async function deleteTodo(id) {
+    try {
+        const queryText = `
+            delete from todos where id = $1 returning * 
+        `;
+        const result = await pool.query(queryText, [id]);
+        return result.rows.length > 0;
+    } catch (error) {
+        throw new Error('Lỗi không xoá được todo list:', error.message);
+    }
+}
---
+++ back-end/src/app/services/user.service.js
@@ -0,0 +1,110 @@
+import pool from '../../configs/postgres.js';
+import bcrypt from 'bcrypt';
+
+// Create new user
+export async function createUser({ username, email, password }) {
+    // const client = await pool.connect();
+    try {
+        const saltRounds = 10;
+        const salt = await bcrypt.genSalt(saltRounds); // ma hoa hash code
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        const queryText = `
+            insert into users (username, email, password)
+            values ($1, $2, $3)
+            returning id, username, email
+        `;
+
+        const result = await pool.query(queryText, [username, email, hashedPassword]);
+        return result.rows[0];
+    } catch (error) {
+        throw new Error('Lỗi tạo người dùng:', error.message);
+    }
+    // finally {
+    //     client.release();
+    // }
+};
+
+// Find user by id 
+export async function findUserById(id) {
+    try {
+        const queryText = 'select * from users where id = $1';
+        const result = await pool.query(queryText, [id]);
+        if (result.rows.length === 0) {
+            return null;
+        }
+        return result.rows[0];
+    } catch (error) {
+        throw new Error(`Lỗi không tim thấy người dùng: ${error.message}`);
+    }
+}
+
+// Find user by email 
+export async function findUserByEmail(email) {
+    try {
+        const queryText = 'select * from users where email = $1';
+        const result = await pool.query(queryText, [email]);
+        if (result.rows.length === 0) {
+            return null;
+        }
+        return result.rows[0];
+    } catch (error) {
+        throw new Error(`Lỗi không tim thấy người dùng: ${error.message}`);
+    }
+}
+
+// Find user by username
+export async function findUserByUsername(username) {
+    try {
+        const queryText = 'select * from users where username = $1';
+        const result = await pool.query(queryText, [username]);
+        if (result.rows.length === 0) {
+            return null;
+        }
+        return result.rows[0];
+    } catch (error) {
+        throw new Error(`Lỗi không tim thấy người dùng: ${error.message}`);
+    }
+}
+
+// Get all user
+export async function getAllUser() {
+    try {
+        const queryText = 'select * from users';
+        const result = await pool.query(queryText);
+        return result.rows;
+    } catch (error) {
+        throw new Error('Lỗi lấy dữ liệu tất cả người dùng:', error.message);
+    }
+}
+
+// Update user information (username, email)
+export async function updateUser(userId, { username, email }) {
+    try {
+        const currentUser = await findUserById(userId);
+        if (!currentUser) {
+            throw new Error('Id không hợp lệ');
+        }
+        const queryText = `
+            update users
+            set username = $1, email = $2
+            where id = $3
+            returning id, username, email
+        `;
+        const result = await pool.query(queryText, [username, email, userId]);
+        return result.rows[0];
+    } catch (error) {
+        throw new Error('Lỗi cập nhật thông tin người dùng:', error.message);
+    }
+}
+
+// Delete user
+export async function deleteUser(userId) {
+    try {
+        const queryText = 'delete from user where id = $1 returning *';
+        const result = await pool.query(queryText, [userId]);
+        return result.rows.length > 0;
+    } catch (error) {
+        throw new Error('Lỗi xoá người dùng:', error.message);
+    }
+}
