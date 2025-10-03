2b48776deb8bc416e9d44e5a87d6bca4db62e85b
Nguyen Thang <nguyenthang7215@gmail.com>
2025-10-04 01:49:46 +0700

    Task 3: Build service layer interact with database

--- back-end/src/app/services/user.service.js
+++ back-end/src/app/services/user.service.js
@@ -28,7 +28,7 @@ export async function createUser({ username, email, password }) {
 // Find user by id 
 export async function findUserById(id) {
     try {
-        const queryText = 'select * from users where id = $1';
+        const queryText = 'select id, username, email from users where id = $1';
         const result = await pool.query(queryText, [id]);
         if (result.rows.length === 0) {
             return null;
@@ -42,7 +42,7 @@ export async function findUserById(id) {
 // Find user by email 
 export async function findUserByEmail(email) {
     try {
-        const queryText = 'select * from users where email = $1';
+        const queryText = 'select id, username, email from users where email = $1';
         const result = await pool.query(queryText, [email]);
         if (result.rows.length === 0) {
             return null;
@@ -56,7 +56,7 @@ export async function findUserByEmail(email) {
 // Find user by username
 export async function findUserByUsername(username) {
     try {
-        const queryText = 'select * from users where username = $1';
+        const queryText = 'select id, username, email from users where username = $1';
         const result = await pool.query(queryText, [username]);
         if (result.rows.length === 0) {
             return null;
@@ -70,7 +70,7 @@ export async function findUserByUsername(username) {
 // Get all user
 export async function getAllUser() {
     try {
-        const queryText = 'select * from users';
+        const queryText = 'select id, username, email from users';
         const result = await pool.query(queryText);
         return result.rows;
     } catch (error) {
@@ -106,5 +106,23 @@ export async function deleteUser(userId) {
         return result.rows.length > 0;
     } catch (error) {
         throw new Error('Lỗi xoá người dùng: ' + error.message);
+    }
+}
+
+// Rest password
+export async function resetPassword(userId, password) {
+    try {
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+        const queryText = `
+            update users 
+            set password = $1 
+            where id = $2
+            returning *
+        `;
+        const result = await pool.query(queryText, [hashedPassword, userId]);
+        return result.rows.length > 0;
+    } catch (error) {
+        throw new Error('Lỗi không thay đổi được mật khẩu: ' + error.message);
     }
 }
