0cc4ac9486c9878cebe1fa08f9659ff6ece82452
Nguyen Thang <nguyenthang7215@gmail.com>
2025-10-03 22:57:18 +0700

    Task 1: Set up database connection

--- back-end/src/configs/postgres.js
+++ back-end/src/configs/postgres.js
@@ -1,28 +1,23 @@
-import { Sequelize } from 'sequelize';
+import { Pool } from 'pg';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
-const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-        host: process.env.DB_HOST,
-        // port: process.env.DB_PORT, // Khong can, mac dinh la cong 5432
-        dialect: 'postgres', // Bat buoc phai chi dinh la postgres
-    }
-);
+const pool = new Pool({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD
+});
 
-async function connectDB() {
-    try {
-        await sequelize.authenticate(); // Thu ket noi toi db
-        console.log('Kết nối thành công tới database!');
-    } catch (error) {
-        console.error('Lỗi kết nối:', error);
-    }
-};
+// Check the connection
+pool.on('connect', () => {
+    console.log('Kết nối thành công tới PostgreSQL Database!');
+});
 
-connectDB;
+pool.on('error', (err) => {
+    console.error('Lỗi kết nối tới PostgreSQL Database:', err);
+});
 
-export default sequelize;
+export default pool;
