7c0809623e1af3593fdd9efe0514ebdfb82b5b3d
Nguyen Thang <nguyenthang7215@gmail.com>
2025-10-05 10:19:17 +0700

    Task 1: Setup database connection

---
+++ src/configs/postgres.js
@@ -0,0 +1,23 @@
+import { Pool } from 'pg';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const pool = new Pool({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD
+});
+
+// Check the connection
+pool.on('connect', () => {
+    console.log('Kết nối thành công tới PostgreSQL Database!');
+});
+
+pool.on('error', (err) => {
+    console.error('Lỗi kết nối tới PostgreSQL Database:', err);
+});
+
+export default pool;
