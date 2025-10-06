f8ecb35d7cd67a2d7de2c779a3ba8b9c705f20c2
Nguyen Thang <nguyenthang7215@gmail.com>
2025-10-06 21:45:37 +0700

    Task 4: Build the controller layer

--- src/app/controllers/auth.controller.js
+++ src/app/controllers/auth.controller.js
@@ -2,7 +2,7 @@ import * as authService from '../services/auth.service.js';
 import * as userService from '../services/user.service.js';
 
 export async function login(req, res) {
-    const validLogin = await authService.checkInvalidLogin(req.body); // email, password
+    const validLogin = await authService.checkValidLogin(req.body); // email, password
     if (validLogin) {
         const result = authService.authToken(validLogin);
         res.json({ result, message: 'Đăng nhập thành công!' });
@@ -20,14 +20,22 @@ export async function register(req, res) {
 
 export async function logout(req, res) {
     const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        res.status(401).json({ message: 'Thiếu token!' });
+    }
     const token = authHeader.split(' ')[1];
     await authService.blockToken(token);
+    res.json({ message: 'Đăng xuất thành công!' })
 }
 
 export async function resetPassword(req, res) {
     const { id, password } = req.body;
     await userService.resetPassword(id, password);
-    authService.blockToken(req.params.token);
+
+    const authHeader = req.headers.authorization;
+    const token = authHeader.split(' ')[1];
+    await authService.blockToken(token);
+    res.json({ message: 'Đổi mật khẩu thành công!' });
 }
 
 
